e23852e6ba6bc898c17d50d3c0d3bb897c163427
Cian Durrigan <cian@adaptis.io>
2018-12-27 15:33:59 +0700

    request data from unsplash axios use asyc await

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  // put async keyword in front of the method name
+  async onSearchSubmit(term) {
+                     // put await keyword in front of whatever is returning (taking time to resolve - network request)  
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID c6ae82e007b0aa434d88702c2bae5069b22869c118f3f42d619e04e2bfd63b8b'
+      }
+    });
+
+    console.log(response.data.results);
+
   }
 
   render() {
